8dca55eda3413bcee54e3437d9c2c18d34c482f1
marcelodoynel <118148280+marcelodoynel@users.noreply.github.com>
2023-11-09 21:58:02 -0300

    Add files via upload

---
+++ vendors/js/productos.js
@@ -0,0 +1,35 @@
+'use strict'
+
+    const prod = [];
+
+    prod[0] = [];
+    prod[0] ['foto'] = 'mataarteria.png';
+    prod[0] ['alt'] = 'Mata arteria';
+    prod[0] ['nombre'] = 'Mata arteria';
+    prod[0] ['estrellas'] = '<span style="font-size: 16px"> <i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi bi-star-fill"></i></span>';
+    prod[0] ['precio'] ='4200';
+    prod[0] ['tost-text'] = 'Preparada con pasión y servida con cariño';
+
+    prod[1] = [];
+    prod[1] ['foto'] = 'ranchera.png';
+    prod[1] ['alt'] = 'Ranchera';
+    prod[1] ['nombre'] = 'Ranchera';
+    prod[1] ['estrellas'] = '<span style="font-size: 16px"> <i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi bi-star-fill"></i></span>';
+    prod[1] ['precio'] ='3500';
+    prod[1] ['tost-text'] = 'Te transportará a un mundo de sabor y satisfacción.';
+
+    prod[2] = [];
+    prod[2] ['foto'] = 'cheeseburger.png';
+    prod[2] ['alt'] = 'Cheeseburguer';
+    prod[2] ['nombre'] = 'Cheeseburguer';
+    prod[2] ['estrellas'] = '<span style="font-size: 16px"> <i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi bi-star-fill"></i></span>';
+    prod[2] ['precio'] ='3500';
+    prod[2] ['tost-text'] = 'Prepárate para un desafío culinario intenso';
+
+    prod[3] = [];
+    prod[3] ['foto'] = 'veggie.png';
+    prod[3] ['alt'] = 'Veggie';
+    prod[3] ['nombre'] = 'Veggie';
+    prod[3] ['estrellas'] = '<span style="font-size: 16px"> <i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi me-1 bi-star-fill"></i><i class="bi bi-star-fill"></i></span>';
+    prod[3] ['precio'] ='2800';
+    prod[3] ['tost-text'] = 'La elección perfecta';
